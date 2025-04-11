1e898a7053229f1fa3edc7d707773e48f824e017
Jason Duquenne <47518779+Jduquenne@users.noreply.github.com>
2025-04-11 18:12:06 +0200

    CLI Github page + actions - first test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const allowedNgrokHost =
-  "2c21-2001-861-205e-cfc0-3098-6b07-3ffe-6975.ngrok-free.app";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/torpheen-arena/",
   server: {
     host: true,
-    allowedHosts: [allowedNgrokHost],
   },
 });
